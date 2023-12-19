e4d1fe3061589cea2b8d5f95fd122117ed9a4bdb
Silence <Silence@Silence>
2023-12-19 13:48:27 +0700

    Aplikasi Hore Ver.1.0

---
+++ src/main/java/com/mycompany/aplikasihore/AplikasiHore.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aplikasihore;
+
+/**
+ *
+ * @author Silence
+ */
+public class AplikasiHore {
+
+    public static void main(String[] args) {
+         MainUi lv = new MainUi();
+            lv.setLocationRelativeTo(null);
+            lv.setVisible(true);      
+    }
+}
